6e3339b838e18a2e68a4bfc63a4663c7d1723d1b
Ben Kozuch <benkozuch@Bens-MBP.attlocal.net>
2022-11-22 17:52:16 -0600

    custom value

--- dapp-frontend/src/App.svelte
+++ dapp-frontend/src/App.svelte
@@ -5,6 +5,8 @@
  
   let connectedAccount = "";
   let contractMessage = "";
+  let new_message = "";
+
   const provider = new ethers.providers.Web3Provider(window.ethereum);
 
   async function connectWallet() {
@@ -21,6 +23,13 @@
     const contract = new ethers.Contract(address, abi, provider);
     contractMessage = await contract.message();
   }
+
+  async function setMessage() {
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, signer);
+    await contract.setMessage(new_message);
+  }
+
 </script>
  
 <main>
@@ -29,4 +38,6 @@
  
   <button on:click={connectWallet}>Connect wallet</button>
   <button on:click={callContract}>Call Contract</button>
+  <button on:click={setMessage}>Set Message</button>
+  <input type="text"  bind:value={new_message}>
 </main>
