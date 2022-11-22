f254cc4e6cdb704f2d69b886aa3b5891b64dfd73
Ben Kozuch <benkozuch@Bens-MBP.attlocal.net>
2022-11-22 17:29:05 -0600

    first commit

---
+++ dapp-frontend/src/contractData.js
@@ -0,0 +1,35 @@
+export const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "message",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_message",
+        type: "string",
+      },
+    ],
+    name: "setMessage",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+export const address = "0x97abf42a07bc9236b90fbd17d4017361d5ae709f";
